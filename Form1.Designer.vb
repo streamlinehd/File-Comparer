3b0fd8dd1749a6966f15411d24cc4b2e124ef30d
Streamlinehd <streamlinehd@gmail.com>
2011-02-04 19:35:08 -0500

    second commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,125 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.dlg = New System.Windows.Forms.OpenFileDialog()
+        Me.SuspendLayout()
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(55, 28)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(253, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(55, 73)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(253, 20)
+        Me.TextBox3.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(17, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(32, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "File1:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(17, 76)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(32, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "File2:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(314, 29)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(36, 19)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "..."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(314, 73)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(36, 20)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "..."
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(55, 108)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(253, 23)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Compare"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'dlg
+        '
+        Me.dlg.FileName = "dlg"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(365, 154)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Name = "Form1"
+        Me.Text = "File Comparer"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents dlg As System.Windows.Forms.OpenFileDialog
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,116 @@
+﻿Option Explicit On
+
+Imports Microsoft.VisualBasic
+Imports System.IO
+
+Public Class Form1
+    Dim file1byte As Integer
+    Dim file2byte As Integer
+    Dim fs1 As FileStream
+    Dim fs2 As FileStream
+    Private Sub OpenNewInputFile()
+
+        fs1 = New IO.FileStream(TextBox3.Text, IO.FileMode.Open)
+        fs1.Close()
+    End Sub
+    Private Sub OpenInputFile()
+
+        fs1 = New IO.FileStream(TextBox2.Text, IO.FileMode.Open)
+        fs1.Close()
+    End Sub
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        With dlg
+            .InitialDirectory = Application.StartupPath
+            .ShowDialog()
+            If .FileName <> "" Then
+                TextBox2.Text = .FileName
+                Call OpenInputFile()
+            End If
+        End With
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        With dlg
+            .InitialDirectory = Application.StartupPath
+            .ShowDialog()
+            If .FileName <> "" Then
+                TextBox3.Text = .FileName
+                Call OpenNewInputFile()
+            End If
+        End With
+    End Sub
+    Private Function FileCompare(ByVal file1 As String, ByVal file2 As String) As Boolean
+        Dim file1byte As Integer
+        Dim file2byte As Integer
+        Dim fs1 As FileStream
+        Dim fs2 As FileStream
+
+        If TextBox2.Text = "" Then
+            Exit Function
+        ElseIf TextBox3.Text = "" Then
+            Exit Function
+        ElseIf TextBox2.Text = TextBox3.Text Then
+            Exit Function
+            fs1.Close()
+            fs1 = Nothing
+            fs2.Close()
+            fs2 = Nothing
+        End If
+        ' Open the two files.
+        fs1 = New FileStream(file1, FileMode.Open)
+        fs2 = New FileStream(file2, FileMode.Open)
+
+        ' Check the file sizes. If they are not the same, the files
+        ' are not equal.
+        If (fs1.Length <> fs2.Length) Then
+            ' Close the file
+            fs1.Close()
+            fs1 = Nothing
+            fs2.Close()
+            fs2 = Nothing
+
+            ' Return a non-zero value to indicate that the files are different.
+            Return False
+        End If
+
+        ' Read and compare a byte from each file until either a
+        ' non-matching set of bytes is found or until the end of
+        ' file1 is reached.
+        Do
+            ' Read one byte from each file.
+            file1byte = fs1.ReadByte()
+            file2byte = fs2.ReadByte()
+        Loop While ((file1byte = file2byte) And (file1byte <> -1))
+
+        ' Close the files.
+        fs1.Close()
+        fs2.Close()
+
+        ' Return the success of the comparison. "file1byte" is
+        ' equal to "file2byte" at this point only if the files are 
+        ' the same.
+        Return ((file1byte - file2byte) = 0)
+    End Function
+
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        ' Compare the two files that are referenced in the textbox controls.
+        If (FileCompare(Me.TextBox2.Text, Me.TextBox3.Text)) Then
+            MessageBox.Show("Files are identical")
+        ElseIf TextBox2.Text = "" Then
+            MsgBox("Please select a file to comapare", MsgBoxStyle.Information + MsgBoxStyle.OkOnly)
+        ElseIf TextBox3.Text = "" Then
+            MsgBox("Please select a file to compare", MsgBoxStyle.Information + MsgBoxStyle.OkOnly)
+        ElseIf TextBox2.Text = TextBox3.Text Then
+            MsgBox("Files are identical", MsgBoxStyle.Information + MsgBoxStyle.OkOnly)
+        Else
+            MessageBox.Show("Files do not match")
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
+
